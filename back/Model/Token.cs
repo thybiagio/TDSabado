579d079e4e31bdfdc44620169e60e215cbb80195
Thyago Biagio <103282998+thybiagio@users.noreply.github.com>
2022-10-01 09:26:53 -0300

    Inicialização do projeto Entity Framework

---
+++ back/Model/Token.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Back.Model
+{
+    public partial class Token
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string Value { get; set; } = null!;
+
+        public virtual Usuario? User { get; set; }
+    }
+}
